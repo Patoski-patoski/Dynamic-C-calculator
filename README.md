77863e59f7f675e35853338c32f94ca99197b4f9
Patrick Okafor Chibuike <Patrickpeters911@gmail.com>
2023-07-12 00:06:34 +0100

    added program to convert numbers to different base

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-### A dynamic Arithemetic calculator made using C programming language
-# Dynamic-C-calculator
+
+# A dynamic Arithemetic calculator made using C programming language
+
+## Dynamic-C-calculator
+
+The dynamic calculator makes simple calculation possible. From your commandline, you could perform arithmetical calculations and convert numbers to any base.
--- converToBase.c
+++ converToBase.c
@@ -2,8 +2,40 @@
 
 void convertToBase()
 {
-    int i;
-    float sum = 0.000;
+    const char baseDigits[17] =
+        {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
-    printf("%f", sum);
+    int convertedNumber[64];
+    int numberToBeConverted = 0;
+    int nextDigit;
+    int base = 0, index = 0;
+
+    do
+    {
+        printf("Enter the number you want to convert: ");
+        scanf("%d", &numberToBeConverted);
+    } while (numberToBeConverted <= 0);
+
+    do
+    {
+        printf("convert to base? ");
+        scanf("%d", &base);
+    } while (base <= 0);
+
+    do
+    {
+        convertedNumber[index] = numberToBeConverted % base;
+        ++index;
+        numberToBeConverted /= base;
+    } while (numberToBeConverted != 0);
+
+    for (index--; index >= 0; index--)
+    {
+        nextDigit = convertedNumber[index];
+        printf("%c", baseDigits[nextDigit]);
+    }
+
+    puts("");
+    return;
 }
